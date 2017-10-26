98f1aa2e7642e73879cb7f8a2e8e3d1bb26e0911
LeoWirz <leonardo.wirz@epfl.ch>
2017-10-26 15:50:19 +0200

    merge JSONs into single dataframe

--- Code/JSONreader.py
+++ Code/JSONreader.py
@@ -1,31 +1,60 @@
-import json, os, codecs, sys
+import json, os, codecs, sys, glob
 import pandas as pd
-import sys
 
-def json_to_csv(DATA_FOLDER="", DESTINATION_FOLDER="", NUMBER_OF_ARTICLES=10):
+def merge_into_dataframe(path):
+
+    allFiles = glob.glob(path + "/*.json")
+    frame = pd.DataFrame()
+    list_ = []
+    for file_ in allFiles:
+        df = json_to_dataframe(file_)
+        list_.append(df)
+    return pd.concat(list_)
+
+
+def json_to_dataframe(DATA_FOLDER):
     """takes NUMBER_OF_ARTICLES (all if it's equal to -1) json files from data_folder and export it as a csv in desination_folder"""
-    json_files = [pos_json for pos_json in os.listdir(DATA_FOLDER) if pos_json.endswith('.json')]
+    df = pd.read_json(DATA_FOLDER)
+ 
+    jsons_data = pd.DataFrame(columns=['site','site_type', 'site_categories', 'domain_rank', 'country', 'author', 'published', 'title',
+                                        'text', 'highlightText', 'highlightTitle', 'language', 'rating',
+                                        'locations_pos', 'locations_neu', 'locations_neg',
+                                        'organizations_pos', 'organizations_neu', 'organizations_neg',
+                                        'persons_pos', 'persons_neu', 'persons_neg'])
 
-    # here I define my pandas Dataframe with the columns I want to get from the json
-    jsons_data = pd.DataFrame(columns=['site_full','site_type', 'published', 'url', 'author', 'title', 'uuid', 'text'])
+    for index,json_text in enumerate(df.posts):
 
-    # we need both the json and an index number so use enumerate()
-    for index, js in enumerate(json_files[:NUMBER_OF_ARTICLES]):
-        json_text = json.load(codecs.open(DATA_FOLDER + '\\' + js, 'r', 'utf-8-sig'))
+        site                = json_text['thread']['site']
+        site_type           = json_text['thread']['site_type']
+        site_categories     = json_text['thread']['site_categories']
+        domain_rank         = json_text['thread']['domain_rank']
+        country             = json_text['thread']['country']
+        author              = json_text['author']
+        published           = json_text['published']
+        title               = json_text['title']
+        text                = json_text['text']
+        highlightText       = json_text['highlightText']
+        highlightTitle      = json_text['highlightTitle']
+        language            = json_text['language']
+        rating              = json_text['rating']
 
-        # here you need to know the layout of your json and each json has to have
-        # the same structure (obviously not the structure I have here
-        site_full = json_text['thread']['site_full']
-        site_type = json_text['thread']['site_type']
-        published = json_text['published']
-        url = json_text['url']
-        author = json_text['author']
-        title = json_text['title']
-        uuid = json_text['uuid']
-        text = json_text['text']
+        locations_pos       = [x['name'] for x in json_text['entities']['locations'] if x['sentiment']=='positive']
+        locations_neu       = [x['name'] for x in json_text['entities']['locations'] if x['sentiment']=='none']
+        locations_neg       = [x['name'] for x in json_text['entities']['locations'] if x['sentiment']=='negative']
+
+        organizations_pos   = [x['name'] for x in json_text['entities']['organizations'] if x['sentiment']=='positive']
+        organizations_neu   = [x['name'] for x in json_text['entities']['organizations'] if x['sentiment']=='none']
+        organizations_neg   = [x['name'] for x in json_text['entities']['organizations'] if x['sentiment']=='negative']
+
+        persons_pos         = [x['name'] for x in json_text['entities']['persons'] if x['sentiment']=='positive']
+        persons_neu         = [x['name'] for x in json_text['entities']['persons'] if x['sentiment']=='none']
+        persons_neg         = [x['name'] for x in json_text['entities']['persons'] if x['sentiment']=='negative']
 
 
-        # here I push a list of data into a pandas DataFrame at row given by 'index'
-        jsons_data.loc[index] = [site_full, site_type, published, url, author, title, uuid, text]
-    jsons_data.to_csv(os.path.join(DESTINATION_FOLDER,r'generatedFromJsons.csv'))
-
+        jsons_data.loc[index] = [site, site_type, site_categories, domain_rank, country, author, published, title,
+                                        text, highlightText, highlightTitle, language, rating,
+                                        locations_pos, locations_neu, locations_neg,
+                                        organizations_pos, organizations_neu, organizations_neg,
+                                        persons_pos, persons_neu, persons_neg]
+    
+    return jsons_data
